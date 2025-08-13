137382588333d5bfb285db63139d995200665cd8
edwinmgallego <ing.edwin.gallego@gmail.com>
2025-08-12 20:52:30 -0500

    haciendo  objetos  desde  javascript

---
+++ DOM/main.js
@@ -0,0 +1,25 @@
+const  v1= "tio ben";
+const  v2= "recueda  lo siguiente";
+const v3= "un gran poder  requiere una  gran responsabolidad"
+const v4 = "ten cuidado con el duende verde";
+
+const spiderman=(peligro)=>{
+    if (peligro == true){
+        return `como decia el ${v1} ${v2} ${v3}`; 
+    }else{
+        return `como decia el ${v1} ${v4}`;
+    }
+
+};
+//etiqueta de  titulos
+const titulo = document.createElement("h1");
+titulo.textContent ="hola Me llamo spiderman";
+document.body.appendChild(titulo);
+
+// crear un boton desde  javascript.
+const boton= document.createElement("button");
+const boton2= document.createElement("button");
+boton.innerText="Peligro";
+boton2.innerText="No peligro";
+document.body.appendChild(boton);
+document.body.appendChild(boton2);
