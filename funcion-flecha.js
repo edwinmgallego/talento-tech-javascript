9b3f8e07211b95d5e744f1c7ff1175ff5f08beca
edwinmgallego <ing.edwin.gallego@gmail.com>
2025-08-12 19:31:20 -0500

    aprendiendo como se hace una funcion flecha

---
+++ funcion-flecha.js
@@ -0,0 +1,16 @@
+function numbreDelaFuncion(n1,n2){
+
+    console.log("los numeros  son :", n1 ,n2)
+}
+
+const variable =()=>{
+
+
+}
+
+const imprimirNombre =(nombre)=>{
+
+    return "hola"+ nombre;
+}
+resultado= imprimirNombre("edwin");
+console.log(resultado);
